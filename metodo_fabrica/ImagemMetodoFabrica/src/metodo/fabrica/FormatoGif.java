e6c6bc74e0dc2f2632d5a430ea9e1a3d4fc22509
GustavoGomesDias <treeelevengg@gmail.com>
2021-10-08 03:25:32 -0300

    feat: add FormatoGif

---
+++ metodo_fabrica/ImagemMetodoFabrica/src/metodo/fabrica/FormatoGif.java
@@ -0,0 +1,10 @@
+package metodo.fabrica;
+
+public class FormatoGif implements Formato {
+
+    @Override
+    public Imagem gera() {
+        System.out.println("formato gif");
+        return null;
+    }
+}
